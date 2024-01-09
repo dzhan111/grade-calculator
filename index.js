74af317bae46dce6bf54dac57d148e4248dd1c91
dzhan111 <zhandavid4@gmail.com>
2024-01-08 23:22:39 -0500

    added final grade calculation, fixed some bugs

--- index.js
+++ index.js
@@ -7,10 +7,7 @@ function addInput() {
 
     newInputDiv.innerHTML = `
         <br>
-        <div>
-            <label for="assignmentName">Assignment: </label>
-            <input type="text" name="assignmentName[]" class="assignmentName" placeholder="(optional)">
-        </div>
+        
         <div class="aligned">
             <label for="grade">Grade:</label>
             <input type="number" name="grade[]" class="grade" min="0" max="100" placeholder="grade (out of 100)">
@@ -33,20 +30,20 @@ function calculateAverage() {
     let totalWeightedScore = 0;
     let totalWeight = 0;
 
-    for (let i = 0; i < assignmentNames.length; i++) {
+    for (let i = 0; i < grades.length; i++) {
         const grade = parseFloat(grades[i].value);
         const weight = parseFloat(weights[i].value);
 
-        if (!isNaN(grade) && !isNaN(weight)) {
+        
             totalWeightedScore += (grade * weight);
             totalWeight += weight;
-        }
+        
     }
 
     const average = totalWeight !== 0 ? totalWeightedScore / totalWeight : 0;
 
     const resultContainer = document.getElementById('result');
-    resultContainer.innerHTML = `Raw Weighted Average: ${average.toFixed(2)} %`;
+    resultContainer.innerHTML = `Raw Weighted Average: ${average} %`;
 }
 
 
@@ -56,3 +53,17 @@ function removeInput(button) {
 }
 
 
+
+function calculateFinal() {
+    const current = document.getElementById("current").value;
+    const target = document.getElementById("target").value;
+    const finalweight = document.getElementById("final-weight").value;
+
+    const result = document.getElementById("result2");
+
+    var curr = (100 - finalweight) * current;
+    var answer = ((target * 100 - curr) / finalweight).toFixed(2);
+
+
+    result.innerHTML = `\nYou need a ${answer}% on the final exam to achieve your target grade.`
+}
